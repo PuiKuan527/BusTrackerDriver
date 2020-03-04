cc7cf30daea13b65db0c8cb3f889999fa38b6525
wilsonkhor1997 <Fastical05250525>
2020-03-04 16:28:26 +0800

    Bus Tracker

--- app/src/main/java/com/test/bustrackerdriver/MapsActivity.java
+++ app/src/main/java/com/test/bustrackerdriver/MapsActivity.java
@@ -5,8 +5,6 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
@@ -14,8 +12,6 @@ import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
-import android.view.View;
-import android.widget.Button;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -39,12 +35,11 @@ import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.karumi.dexter.Dexter;
 import com.karumi.dexter.PermissionToken;
 import com.karumi.dexter.listener.PermissionDeniedResponse;
@@ -57,18 +52,16 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GeoQueryEventListener {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GeoQueryEventListener, IOnLoadLocationListener {
 
     private GoogleMap mMap;
     private LocationRequest locationRequest;
     private LocationCallback locationCallback;
     private FusedLocationProviderClient fusedLocationProviderClient;
-    private Marker currentUser;
+    private Marker currentuser;
     private DatabaseReference myLocationRef;
     private GeoFire geoFire;
     private List<LatLng> busStop;
-    private Button logout;
-
     private IOnLoadLocationListener listener;
 
     private DatabaseReference myBusStop;
@@ -79,19 +72,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
-        logout=findViewById(R.id.logout);
-
-        logout.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MapsActivity.this, LoginActivity.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                SharedPreferences sharedPreferences = MapsActivity.this.getSharedPreferences("wilson", Context.MODE_PRIVATE);
-                SharedPreferences.Editor edit = sharedPreferences.edit();
-                edit.clear().commit();
-                startActivity(intent);
-            }
-        });
 
         Dexter.withActivity(this)
                 .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
@@ -100,13 +80,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     public void onPermissionGranted(PermissionGrantedResponse response) {
                         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
 
-                        buildLoacationRequest();
+                        buildLocationRequest();
                         buildLocationCallback();
-                        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(MapsActivity.this);
-
-                        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                                .findFragmentById(R.id.map);
-                        mapFragment.getMapAsync(MapsActivity.this);
+                        fusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(MapsActivity.this);
 
                         initArea();
                         settingGeoFire();
@@ -114,8 +90,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
                     @Override
                     public void onPermissionDenied(PermissionDeniedResponse response) {
-                        Toast.makeText(MapsActivity.this,"You Must enable permission",Toast.LENGTH_SHORT).show();
-
+                        Toast.makeText(MapsActivity.this,"You must enable permission",Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
@@ -127,93 +102,114 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     }
 
     private void initArea() {
-        busStop = new ArrayList<>();
+        myBusStop=FirebaseDatabase.getInstance()
+                .getReference("BusStopArea")
+                .child("BusStop");
+        listener=this;
 
-        busStop.add(new LatLng(6.462894,100.504288));
-        busStop.add(new LatLng(6.467529,100.506804));
 
-//        busStop.add(new LatLng(6.470418,100.498405));
-//        busStop.add(new LatLng(6.467850,100.498529));
-//        busStop.add(new LatLng(6.465787,100.499307));
-//        busStop.add(new LatLng(6.465787,100.499983));
-//        busStop.add(new LatLng(6.462653,100.500605));
-//        busStop.add(new LatLng(6.467626,100.496372));
-//        busStop.add(new LatLng(6.459440,100.501740));
-//        busStop.add(new LatLng(6.458191,100.502320));
-//        busStop.add(new LatLng(6.456869,100.503795));
-//        busStop.add(new LatLng(6.458753,100.506736));
-//        busStop.add(new LatLng(6.458876,100.505932));
-//        busStop.add(new LatLng(6.455068,100.506305));
-//        busStop.add(new LatLng(6.455164,100.508209));
-//        busStop.add(new LatLng(6.452927,100.499855));
-//        busStop.add(new LatLng(6.464031,100.508509));
-//        busStop.add(new LatLng(6.465362,100.508645));
-//        busStop.add(new LatLng(6.465435,100.508852));
-//        busStop.add(new LatLng(6.467380,100.509307));
-//        busStop.add(new LatLng(6.471002,100.506748));
-//        busStop.add(new LatLng(6.478570,100.508437));
-
-        FirebaseDatabase.getInstance()
-                .getReference("Bus Stop")
-                .push()
-                .setValue(busStop)
-                .addOnCompleteListener(new OnCompleteListener<Void>() {
-                    @Override
-                    public void onComplete(@NonNull Task<Void> task) {
-                        Toast.makeText(MapsActivity.this, "Updated!", Toast.LENGTH_SHORT).show();
-                    }
-                }).addOnFailureListener(new OnFailureListener() {
+//                myBusStop.addListenerForSingleValueEvent(new ValueEventListener() {
+//                    @Override
+//                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+//                        List<MyLatLng> latLngList = new ArrayList<>();
+//                        for (DataSnapshot locationSnapShot: dataSnapshot.getChildren()){
+//                            MyLatLng latLng = locationSnapShot.getValue(MyLatLng.class);
+//                            latLngList.add(latLng);
+//                        }
+//                        listener.OnLoadLocationSuccess(latLngList);
+//                    }
+//
+//                    @Override
+//                    public void onCancelled(@NonNull DatabaseError databaseError) {
+//                        listener.OnLoadLocationFailed(databaseError.getMessage());
+//                    }
+//                });
+        myBusStop.addValueEventListener(new ValueEventListener() {
             @Override
-            public void onFailure(@NonNull Exception e) {
-                Toast.makeText(MapsActivity.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                List<MyLatLng> latLngList = new ArrayList<>();
+                for (DataSnapshot locationSnapShot: dataSnapshot.getChildren()){
+                    MyLatLng latLng = locationSnapShot.getValue(MyLatLng.class);
+                    latLngList.add(latLng);
+                }
+
+                listener.OnLoadLocationSuccess(latLngList);
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
+
+
+//        FirebaseDatabase.getInstance()
+//                .getReference("BusStopArea")
+//                .child("BusStop")
+//                .setValue(busStop)
+//                .addOnCompleteListener(new OnCompleteListener<Void>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<Void> task) {
+//                        Toast.makeText(MapsActivity.this,"Updated!",Toast.LENGTH_SHORT).show();
+//                    }
+//                }).addOnFailureListener(new OnFailureListener() {
+//            @Override
+//            public void onFailure(@NonNull Exception e) {
+//                Toast.makeText(MapsActivity.this,""+e.getMessage(),Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+    }
+
+    private void settingGeoFire() {
+        myLocationRef= FirebaseDatabase.getInstance().getReference("MyLocation");
+        geoFire=new GeoFire(myLocationRef);
+    }
+
+    private void buildLocationCallback() {
+        locationCallback=new LocationCallback(){
+            @Override
+            public void onLocationResult(final LocationResult locationResult) {
+                if (mMap != null)
+                {
+                    lastLocation=locationResult.getLastLocation();
+                    addUserMarker();
+
+                }
+            }
+        };
+
+    }
+
+    private void addUserMarker() {
+        geoFire.setLocation("You", new GeoLocation(lastLocation.getLatitude(),
+                lastLocation.getLongitude()), new GeoFire.CompletionListener() {
+            @Override
+            public void onComplete(String key, DatabaseError error) {
+                if(currentuser != null) currentuser.remove();
+                currentuser = mMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(lastLocation.getLatitude(),
+                                lastLocation.getLongitude()))
+                        .title("You"));
+
+                mMap.animateCamera(CameraUpdateFactory
+                        .newLatLngZoom(currentuser.getPosition(),17.5f));
+
             }
         });
     }
 
-    private void settingGeoFire() {
-
-        myLocationRef = FirebaseDatabase.getInstance().getReference("MyLocation");
-        geoFire = new GeoFire(myLocationRef);
-
-    }
-
-    private void buildLoacationRequest() {
-        locationRequest = new LocationRequest();
+    private void buildLocationRequest() {
+        locationRequest=new LocationRequest();
         locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        locationRequest.setInterval(8000);
-        locationRequest.setFastestInterval(5000);
+        locationRequest.setInterval(5000);
+        locationRequest.setFastestInterval(3000);
         locationRequest.setSmallestDisplacement(10f);
     }
-
-    private void buildLocationCallback() {
-        locationCallback = new LocationCallback(){
-            @Override
-            public void onLocationResult(final LocationResult locationResult) {
-                if(mMap != null)
-                {
-                    geoFire.setLocation("You", new GeoLocation(locationResult.getLastLocation().getLatitude(),
-                            locationResult.getLastLocation().getLongitude()), new GeoFire.CompletionListener() {
-                        @Override
-                        public void onComplete(String key, DatabaseError error) {
-
-                            if(currentUser != null) currentUser.remove();
-                            currentUser = mMap.addMarker(new MarkerOptions()
-                                    .position(new LatLng(locationResult.getLastLocation().getLatitude(),
-                                            locationResult.getLastLocation().getLongitude()))
-                                    .title("You"));
-                            //After add marker, move camera
-                            mMap.animateCamera(CameraUpdateFactory
-                                    .newLatLngZoom(currentUser.getPosition(),17.5f));
-
-                        }
-                    });
-                }
-            }
-        };
-    }
-
-
-
 
     @Override
     public void onMapReady(GoogleMap googleMap) {
@@ -222,29 +218,33 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap.getUiSettings().setZoomControlsEnabled(true);
 
         if (fusedLocationProviderClient != null)
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                if(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_DENIED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_DENIED){
                     return;
                 }
             }
-        fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback, Looper.myLooper());
+        fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback,Looper.myLooper());
 
-        //Add circle for bus stop
-        for(LatLng latLng : busStop)
-        {
+        addCirleArea();
+    }
+
+    private void addCirleArea() {
+        if(geoquery!=null) {
+            geoquery.removeGeoQueryEventListener(this);
+            geoquery.removeAllListeners();
+        }
+        for (LatLng latLng:busStop){
             mMap.addCircle(new CircleOptions().center(latLng)
-                    .radius(5)    // 5m
+                    .radius(5.0f)
                     .strokeColor(Color.BLUE)
-                    .fillColor(0x220000FF)  // 22 is transparent
+                    .fillColor(0x220000FF)
                     .strokeWidth(5.0f)
             );
 
-            //Create GeoQuery when user in bus stop location
-            GeoQuery geoQuery = geoFire.queryAtLocation(new GeoLocation(latLng.latitude,latLng.longitude),0.5f); // 500m
-            geoQuery.addGeoQueryEventListener(MapsActivity.this);
+            geoquery = geoFire.queryAtLocation(new GeoLocation((latLng.latitude),latLng.longitude),0.1f);
+            geoquery.addGeoQueryEventListener(MapsActivity.this);
 
         }
-
     }
 
     @Override
@@ -255,17 +255,18 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onKeyEntered(String key, GeoLocation location) {
-        sendNotification("UUM Bus Tracker",String.format("%s entered the bus stop area",key));
+        //Toast.makeText(this,"You entered the bus stop area",Toast.LENGTH_LONG).show();
+        sendNotification("Wilson",String.format("%s entered the bus stop area",key));
     }
 
     @Override
     public void onKeyExited(String key) {
-        sendNotification("UUM Bus Tracker",String.format("%s leave the bus stop area",key));
+        sendNotification("Wilson",String.format("%s exited the bus stop area",key));
     }
 
     @Override
     public void onKeyMoved(String key, GeoLocation location) {
-        sendNotification("UUM Bus Tracker",String.format("%s move within the bus stop area",key));
+        sendNotification("Wilson",String.format("%s move within the bus stop area",key));
     }
 
     @Override
@@ -275,39 +276,64 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     @Override
     public void onGeoQueryError(DatabaseError error) {
-        Toast.makeText(this,""+error.getMessage(),Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, ""+error.getMessage(),Toast.LENGTH_SHORT).show();
     }
-
 
     private void sendNotification(String title, String content) {
 
-        Toast.makeText(this,""+content,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, ""+content, Toast.LENGTH_SHORT).show();
+        String NOTIFICATION_CHANNEL_ID="wilson_multiple_location";
+        NotificationManager notificationManager=(NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
 
-        String NOTIFICATION_CHANNEL_ID = "UUM_multiple_location";
-        NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
-
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.O)
+        {
             NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, "My Notification",
                     NotificationManager.IMPORTANCE_DEFAULT);
 
-            //Config
             notificationChannel.setDescription("Channel description");
             notificationChannel.enableLights(true);
             notificationChannel.setLightColor(Color.RED);
-            notificationChannel.setVibrationPattern(new long[]{0,1000,500,1000});
+            notificationChannel.setVibrationPattern(new long [] {0,1000,500,1000});
             notificationChannel.enableVibration(true);
             notificationManager.createNotificationChannel(notificationChannel);
         }
 
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
+        NotificationCompat.Builder builder= new NotificationCompat.Builder(this,NOTIFICATION_CHANNEL_ID);
         builder.setContentTitle(title)
                 .setContentText(content)
                 .setAutoCancel(false)
                 .setSmallIcon(R.mipmap.ic_launcher)
                 .setLargeIcon(BitmapFactory.decodeResource(getResources(),R.mipmap.ic_launcher));
 
-        Notification notification = builder.build();
+        Notification notification=builder.build();
         notificationManager.notify(new Random().nextInt(),notification);
     }
 
+    @Override
+    public void OnLoadLocationSuccess(List<MyLatLng> latLngs) {
+        busStop=new ArrayList<>();
+
+        for(MyLatLng myLatLng:latLngs)
+        {
+            LatLng convert=new LatLng(myLatLng.getLatitude(),myLatLng.getLongitude());
+            busStop.add(convert);
+        }
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MapsActivity.this);
+
+        if (mMap!=null){
+            mMap.clear();
+
+            addUserMarker();
+
+            addCirleArea();
+        }
+    }
+
+    @Override
+    public void OnLoadLocationFailed(String Message) {
+        Toast.makeText(this,""+Message,Toast.LENGTH_SHORT).show();
+    }
 }
